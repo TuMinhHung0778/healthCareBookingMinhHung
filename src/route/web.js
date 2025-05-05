53bf47c3321ad7391956afd4128b84828f78478c
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-05-05 14:50:18 +0700

    add full project source code

---
+++ src/route/web.js
@@ -0,0 +1,13 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoues = (app) => {
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+    // rest api 
+    return app.use("/", router);
+}
+
+module.exports = initWebRoues;
